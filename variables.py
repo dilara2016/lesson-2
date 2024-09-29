d2f2f44218a40e86ca88584dc8494d2aac274f4e
dilara2016 <m.123.hunain@gmail.com>
2024-09-29 11:36:03 +0200

    varibles

---
+++ variables.py
@@ -0,0 +1,13 @@
+#Creating difrent varibles
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User input'
+name = input("enter name:")
+
+print("\nHello",name, "\nwellcome to codingal")
